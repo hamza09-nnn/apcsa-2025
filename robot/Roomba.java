46d7ddf37bd24b1da810dd65862b983fbf00a89f
hamza09-nnn <hammycomedy@gmail.com>
2025-09-08 22:10:36 +0000

    use methods to make code readable

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -4,10 +4,10 @@ import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-	// Main method to make this self-contained
 	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
+		
 		String worldName = "robot/basicRoom.wld";
+		World.setDelay(0);
 
 		Roomba cleaner = new Roomba();
 		int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
@@ -15,55 +15,95 @@ public class Roomba implements Directions {
 
 	}
 
-	// declared here so it is visible in all the methods!
 	private Robot roomba;
-
-	// You will need to add many variables!!
-
 
 	public int cleanRoom(String worldName, int startX, int startY) {
 
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
-
+		
 		World.readWorld(worldName);
 		World.setVisible(true);
 		roomba = new Robot(startX, startY, East, 0);
 
 		int totalBeepers = 0;
+		Direction cleaningDirection = East;
+		boolean doneCleaning = false;
+		
+		while (!doneCleaning) {
 
+			totalBeepers = totalBeepers + cleanStreet();
+
+			//Roomba face direction North
+			faceDirection(North);
+
+			if( roomba.frontIsClear() ) {
+				// move up
+				roomba.move();
+				
+				//Flip the horizontal cleaning direction
+				if( cleaningDirection == East )
+					cleaningDirection = West;
+				else
+					cleaningDirection = East;
+				
+				//Turn roomba to face cleaning direction
+				faceDirection(cleaningDirection);			
+			} 
+			else {
+				doneCleaning = true;
+			}
+
+		} 
+			
+		return totalBeepers;
+		}
+		
+	
+	
+	
+	//Cleans street roomba is on
+	public int cleanStreet() {
+		int beepers = 0;
+		
 		while (roomba.frontIsClear()) {
 
 			while (roomba.nextToABeeper()) {
 				roomba.pickBeeper();
-				totalBeepers++;
+				beepers++;
 			}
 			
 			roomba.move();
 		}
-		//clean last square
-		while (roomba.nextToABeeper()) {
+		//Cleans any beepers on the last avenue on that street
+		 while (roomba.nextToABeeper()) {
 			roomba.pickBeeper();
-			totalBeepers++;
+			beepers++;
+			
 		}
 		
-		return totalBeepers;
-	}	
-
-
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
 		
-		
+		return beepers;
 
-
-		 // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
 		
 	}
 
+	public void faceDirection(Direction d)
+	{
+		if (d == East ) {
+			while( ! roomba.facingEast())
+				roomba.turnLeft();
+		} else if ( d== West ) {
+			while( ! roomba.facingWest())
+				roomba.turnLeft();
+
+		} else if ( d== North ) {
+			while( ! roomba.facingNorth())
+				roomba.turnLeft();
+
+		} else if ( d== South ) {
+			while( ! roomba.facingSouth())
+				roomba.turnLeft();			
+		}
+	} 
+	
+	
+}
