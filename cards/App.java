76d66812a47e9b313f953ad480a01881bcba2378
hamza09-nnn <hammycomedy@gmail.com>
2025-12-12 06:58:25 +0000

    Coded the Deck class. Used random.nextInt method to shuffle the card deck. Tested the code by using the test statements given in the frq instructions and putting them into the app class.  // A deck is constructed in the default order         System.out.println("Creating a new deck...");         Deck d = new Deck();         // print the first 5 cards in the deck - expcted "DA D2 D3 D4 D5"         d.print(5);         System.out.println();
            Card top = d.draw();
            System.out.println("top card: " + top);
            top = d.draw();
            System.out.println("next card: " + top);
    
            // shuffle the deck
            System.out.println("Shuffling the deck...");
            d.shuffle();
    
            // print output should show values different than the default order
            d.print(5);
            System.out.println();
    
            // A new deck constructed in the default order
            System.out.println("Creating a new deck...");
            d = new Deck();
    
            // print the first 5 cards in the deck - expcted "DA D2 D3 D4 D5"
            d.print(5);
            System.out.println();
    
            // cut at index 2
            // DA, D2 -> moved to the end of the deck
            System.out.println("Cutting the deck at index 2...");
            d.cut(2);
    
            // print the first 5 cards in the deck - expected "D3 D4 D5 D6 D7"
            System.out.println("Five cards after cut at index 2:");
            d.print(5);
            System.out.println();
    Output of test :
    ******* Testing Deck class *******
    Creating a new deck...
    ♦A ♦2 ♦3 ♦4 ♦5
    top card: ♦A
    next card: ♦2
    Shuffling the deck...
    ♦9 ♠4 ♣8 ♠J ♦2
    Creating a new deck...
    ♦A ♦2 ♦3 ♦4 ♦5
    Cutting the deck at index 2...
    Five cards after cut at index 2:
    ♦3 ♦4 ♦5 ♦6 ♦7
     --- Rules / How to play ---
    s to stand, h to hit
    Player - Score: 15 Hand: ♥6 ♠9
    Dealer - Score: 11 Hand: ♣8 ♠3
    Enter a command: (q to quit)

--- cards/App.java
+++ cards/App.java
@@ -5,16 +5,58 @@ public class App {
    public static void main(String[] args) {
      // *******  test Card class *******
 
-        Card c = new Card(1,10);
+        /*Card c = new Card(1,10);
         int value = c.getValue();
         System.out.println("value is: " + value);
         
         String str = c.toString();
         System.out.println("card is: " + str);
+        */
+       // ****** test Deck class *******
+        /*System.out.println();
+        System.out.println("******* Testing Deck class *******");
 
-        //CardTable table = new CardTable();
+        // A deck is constructed in the default order
+        System.out.println("Creating a new deck...");
+        Deck d = new Deck(); 
+        // print the first 5 cards in the deck - expcted "DA D2 D3 D4 D5"
+        d.print(5);
+        System.out.println();
 
-       // table.startPlaying();
+        Card top = d.draw();
+        System.out.println("top card: " + top);
+        top = d.draw();
+        System.out.println("next card: " + top);
+
+        // shuffle the deck
+        System.out.println("Shuffling the deck...");
+        d.shuffle();
+
+        // print output should show values different than the default order
+        d.print(5);
+        System.out.println();
+
+        // A new deck constructed in the default order
+        System.out.println("Creating a new deck...");
+        d = new Deck();
+
+        // print the first 5 cards in the deck - expcted "DA D2 D3 D4 D5"
+        d.print(5);
+        System.out.println();
+
+        // cut at index 2 
+        // DA, D2 -> moved to the end of the deck
+        System.out.println("Cutting the deck at index 2...");
+        d.cut(2);
+
+        // print the first 5 cards in the deck - expected "D3 D4 D5 D6 D7"
+        System.out.println("Five cards after cut at index 2:");
+        d.print(5);
+        System.out.println();*/
+
+        CardTable table = new CardTable();
+
+        table.startPlaying();
     }
 
 }
--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,99 @@
 package cards;
 
+import java.util.Random;
+
 public class Deck {
+    private Card[] cards;
+    private int topCardIndex; // index of the next card to deal
 
+    public Deck() {
+        cards = new Card[52];
 
+        int index = 0;
+        for (int suit = 0; suit < 4; suit++) {
+            for (int value = 0; value < 13; value++) {
+                cards[index] = new Card(suit, value);
+                index++;
+            }
+        }
+        topCardIndex = 0;
+    }
+
+    public void print(int numCards) {
+        for (int i = 0; i < numCards && i < cards.length; i++) {
+            // print the card and a space  
+            System.out.print(cards[i].toString());
+            if (i < numCards - 1 && i < cards.length - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public void shuffle() {
+       Random random = new Random();
+
+       // iterate through each card in the deck
+       for( int i=0; i < cards.length; i++) {
+            // generate a random index between 0 and 51
+            int randomIndex = random.nextInt(cards.length);
+
+            // swap the cards at index i and randomIndex
+            swapCards(i, randomIndex);
+        }
+    } // end shuffle
+
+    private void swapCards(int index1, int index2) {
+        // System.out.println("Swapping cards at index " + index1 + " and " + index2);
+        // System.out.println("Before swap: " + cards[index1] + " <-> " + cards[index2]);
+
+        Card temp = cards[index1];
+        cards[index1] = cards[index2];
+        cards[index2] = temp;
+        
+        // System.out.println("After swap: " + cards[index1] + " <-> " + cards[index2]);
+    }// end swapCards
+
+    // cut the deck at the specified index
+    // cards at positions from the top of the deck up to, but not including cutIndex 
+    // are moved to the end of the deck.
+    // pre-condition: works only on the complete deck of 52 cards
+    public void cut(int cutIndex){
+
+        if( cards.length != 52 ){
+            return; // do nothing if the deck is not complete
+        }
+
+        // check if cutIndex is valid to prevent out of bounds error
+        if( cutIndex < 0 || cutIndex >= cards.length ){
+            return; // invalid cut index, do nothing
+        }
+
+        // move cards from the top of the deck up to, but not including cutIndex 
+        // to the end of the deck.
+
+        
+        for( int i=0; i < cutIndex; i++ ){
+            Card temp = cards[0];
+            // shift all cards one position to the left 
+            for( int j=0; j < cards.length - 1; j++ ){
+                cards[j] = cards[j+1];
+            }
+            // place the removed card at the end of the deck
+            cards[cards.length - 1] = temp;
+        }
+
+    }
+
+    // draw returns the card at the top of the deck
+    // subsequennt calls to draw shall return subsequent cards
+    // when the deck is empty, draw shall return null
+    public Card draw() {
+        if (topCardIndex < cards.length) {
+            return cards[topCardIndex++];
+        } else {
+            return null; // no more cards to deal
+        }
+    }
+
+    
 }
