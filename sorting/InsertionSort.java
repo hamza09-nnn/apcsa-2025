7afa82784bd099d0f70165ed0609da1ad3c7b0e0
hamza09-nnn <hammycomedy@gmail.com>
2026-02-19 22:19:28 +0000

    Implemented SelectionSort with step counter

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -2,7 +2,28 @@ package sorting;
 
 public class InsertionSort implements Sorter {
 
+    public static long steps = 0;
+
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+
+        steps = 0;
+
+        for (int i = 1; i < input.length; i++) {
+
+            int key = input[i];
+            int j = i - 1;
+
+            while (j >= 0 && input[j] > key) {
+                steps++; // comparison
+                input[j + 1] = input[j];
+                j--;
+                steps++; // shift
+            }
+
+            input[j + 1] = key;
+            steps++; // insertion
+        }
+
+        System.out.println("InsertionSort steps: " + steps);
     }
 }
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,7 +2,29 @@ package sorting;
 
 public class SelectionSort implements Sorter {
 
+    public static long steps = 0;
+
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+
+        steps = 0;
+
+        for (int i = 0; i < input.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < input.length; j++) {
+                steps++; // comparison step
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = input[i];
+            input[i] = input[minIndex];
+            input[minIndex] = temp;
+            steps++; // swap step
+        }
+
+        System.out.println("SelectionSort steps: " + steps);
     }
 }
